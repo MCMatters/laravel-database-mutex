b61e71fde2a363e7f11a2810ac6ce0e19451fe4c
Dmitry Borzyonok <dima.matters@gmail.com>
2018-12-23 14:20:06 +0200

    Add Mutex classes for console

---
+++ src/Console/Scheduling/DatabaseEventMutex.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace McMatters\LaravelDatabaseMutex\Console\Scheduling;
+
+use Carbon\Carbon;
+use Illuminate\Console\Scheduling\Event;
+use Illuminate\Console\Scheduling\EventMutex;
+use McMatters\LaravelDatabaseMutex\Contracts\DatabaseMutexManagerContract as DatabaseMutexManager;
+
+/**
+ * Class DatabaseEventMutex
+ *
+ * @package McMatters\LaravelDatabaseMutex\Console\Scheduling
+ */
+class DatabaseEventMutex implements EventMutex
+{
+    /**
+     * @var \McMatters\LaravelDatabaseMutex\Contracts\DatabaseMutexManagerContract
+     */
+    protected $manager;
+
+    /**
+     * CacheEventMutex constructor.
+     *
+     * @param \McMatters\LaravelDatabaseMutex\Contracts\DatabaseMutexManagerContract $manager
+     */
+    public function __construct(DatabaseMutexManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param \Illuminate\Console\Scheduling\Event $event
+     *
+     * @return bool
+     */
+    public function create(Event $event): bool
+    {
+        return (bool) $this->manager->create(
+            $event->mutexName(),
+            Carbon::now()->addSeconds($event->expiresAt)
+        );
+    }
+
+    /**
+     * @param \Illuminate\Console\Scheduling\Event $event
+     *
+     * @return bool
+     */
+    public function exists(Event $event): bool
+    {
+        return $this->manager->exists($event->mutexName());
+    }
+
+    /**
+     * @param  \Illuminate\Console\Scheduling\Event $event
+     *
+     * @return void
+     */
+    public function forget(Event $event): void
+    {
+        $this->manager->forget($event->mutexName());
+    }
+}
---
+++ src/Console/Scheduling/DatabaseSchedulingMutex.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace McMatters\LaravelDatabaseMutex\Console\Scheduling;
+
+use Carbon\Carbon;
+use DateTimeInterface;
+use Illuminate\Console\Scheduling\Event;
+use Illuminate\Console\Scheduling\SchedulingMutex;
+use McMatters\LaravelDatabaseMutex\Contracts\DatabaseMutexManagerContract as DatabaseMutexManager;
+
+/**
+ * Class DatabaseSchedulingMutex
+ *
+ * @package McMatters\LaravelDatabaseMutex\Console\Scheduling
+ */
+class DatabaseSchedulingMutex implements SchedulingMutex
+{
+    /**
+     * @var \McMatters\LaravelDatabaseMutex\Contracts\DatabaseMutexManagerContract
+     */
+    protected $manager;
+
+    /**
+     * CacheEventMutex constructor.
+     *
+     * @param \McMatters\LaravelDatabaseMutex\Contracts\DatabaseMutexManagerContract $manager
+     */
+    public function __construct(DatabaseMutexManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @param \Illuminate\Console\Scheduling\Event $event
+     * @param \DateTimeInterface $time
+     *
+     * @return bool
+     */
+    public function create(Event $event, DateTimeInterface $time): bool
+    {
+        return (bool) $this->manager->create(
+            $event->mutexName().$time->format('Hi'),
+            Carbon::parse('+1 hour')
+        );
+    }
+
+    /**
+     * @param \Illuminate\Console\Scheduling\Event $event
+     * @param \DateTimeInterface $time
+     *
+     * @return bool
+     */
+    public function exists(Event $event, DateTimeInterface $time): bool
+    {
+        return $this->manager->exists($event->mutexName().$time->format('Hi'));
+    }
+}
